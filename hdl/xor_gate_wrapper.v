24b0ebfd58a67b7fb326242cac2887185e9c9063
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-02 21:18:29 +0530

    Update xor_gate_wrapper.v

--- hdl/xor_gate_wrapper.v
+++ hdl/xor_gate_wrapper.v
@@ -1,17 +1,11 @@
-// File: hdl/xor_gate_wrapper.v
+// xor_gate_wrapper.v
 module xor_gate_wrapper(
-    input wire clk,
-    input wire reset_n,
-    input wire [1:0] inputs,
-    output wire result
+    input wire a,
+    input wire b,
+    output wire y
 );
-    xor_gate dut (
-        .a(inputs[0]),
-        .b(inputs[1]),
-        .y(result)
-    );
+    xor_gate dut(.a(a), .b(b), .y(y));
     
-    // For waveform dumping
     initial begin
         $dumpfile("waveforms.vcd");
         $dumpvars(0, xor_gate_wrapper);
