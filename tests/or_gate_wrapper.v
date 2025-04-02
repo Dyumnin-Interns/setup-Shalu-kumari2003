f55195cc9169b497c8122b5880e41f06cad71a5f
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-02 20:29:33 +0530

    or_gate_wrapper.v

--- tests/or_gate_wrapper.v
+++ tests/or_gate_wrapper.v
@@ -1,8 +1,12 @@
-module or_gate_wrapper (input wire a, input wire b, output wire y);
-or_gate or_gate(.a(a), .b(b), .y(y));
-
-initial begin
-  $dumpfile("waves.vcd");
-  $dumpvars;
-end  
-endmodule  
+module dut(  // Changed name from or_gate_wrapper to dut
+    input wire a,
+    input wire b,
+    output wire y
+);
+    or_gate or_gate_inst(.a(a), .b(b), .y(y));
+    
+    initial begin
+        $dumpfile("waves.vcd");
+        $dumpvars;
+    end
+endmodule 
