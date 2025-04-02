8013f253887cd2246389629db43ea027c5c23cf1
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-02 19:57:24 +0530

    Update or_gate_wrapper.v

--- tests/or_gate_wrapper.v
+++ tests/or_gate_wrapper.v
@@ -1,5 +1,5 @@
-module xor_gate_wrapper (input wire a, input wire b, output wire y);
-dut dut(.a(a), .b(b), .y(y));
+module or_gate_wrapper (input wire a, input wire b, output wire y);
+or_gate or_gate(.a(a), .b(b), .y(y));
 
 initial begin
   $dumpfile("waves.vcd");
